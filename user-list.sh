0b3309cc00eac536aa64d798385ceed629f6df9f
sugi nn <sugiantonugraha@gmail.com>
2017-07-15 16:55:35 +0700

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -15,4 +15,4 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo -e "Mod by Bustami Arifin"
+echo -e "by sugi nn"
